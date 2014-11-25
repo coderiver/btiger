857a725358216c33f07787e2e398b81973d8b13a
etna-scorpio <etna.scorpio@gmail.com>
2014-11-25 14:07:46 +0200

    fixes 2

--- js/common.js
+++ js/common.js
@@ -1,8 +1,33 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	//cycle progress bar
+	var progress = $('.progress-bar'),
+		slideshow = $( '.cycle-slideshow' );
 
-	console.log($('body').html());
+	slideshow.on( 'cycle-initialized cycle-before', function( e, opts ) {
+		progress.stop(true).css( 'width', 0 );
+	});
+
+	slideshow.on( 'cycle-initialized cycle-after', function( e, opts ) {
+		if ( ! slideshow.is('.cycle-paused') )
+			progress.animate({ width: '100%' }, opts.timeout, 'linear' );
+	});
+
+	slideshow.on( 'cycle-paused', function( e, opts ) {
+		progress.stop();
+	});
+
+	slideshow.on( 'cycle-resumed', function( e, opts, timeoutRemaining ) {
+		progress.animate({ width: '100%' }, timeoutRemaining, 'linear' );
+	});
+
+	//cycle keyboard
+	$(document.documentElement).keyup(function (event) {
+			if (event.keyCode == 37) {
+				$('.cycle-slideshow').cycle('prev');
+			} else if (event.keyCode == 39) {
+				$('.cycle-slideshow').cycle('next')
+		}
+	});
+
 });
